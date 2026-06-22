765b61e1f4d82a1a595b5c5396e6c8689c2c1206
Abdur-Rahim-bin-Bakkar <webdesignrahim4061@gmail.com>
2026-06-22 13:06:29 +0600

    only secure trainer page

--- src/lib/patch/updateClass.js
+++ src/lib/patch/updateClass.js
@@ -1,9 +1,16 @@
+'use server'
+
+import { authHeader } from "../header/header";
+
 export const updateClass = async (id, data) => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URI}/classes/${id}`,
     {
       method: "PATCH",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        ...(await authHeader()),
+      },
       body: JSON.stringify(data),
     }
   );
